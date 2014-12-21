21e43234133449ce1ebdcc11f28c68978674d768
Nicholas Tang <nicholas.yjtang@gmail.com>
2014-12-21 15:42:11 +0800

    Added new information on the data Changed input box to textarea

--- ui.R
+++ ui.R
@@ -7,7 +7,8 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
       conditionalPanel(
         'input.tabs=="Prediction"',
-        textInput(inputId="spamtext", label = "Insert your spam text here", value="Sample spam text"),
+        HTML('<label for="spamtext">Insert your spam text here</label>'),
+        tags$textarea(id="spamtext", rows=5,"Sample spam text"),      
         actionButton("goPredict", "Predict")      
       ),
       conditionalPanel(
@@ -34,13 +35,29 @@ shinyUI(pageWithSidebar(
                  br(),
                  h2("Prediction output:"),
                  br(),
-                 div(textOutput("predictionresult"), style="color:blue;font-size=200%")
+                 div(textOutput("predictionresult"), style="color:blue;font-size=2.5em")
                  ),
         tabPanel("Input information",
                  div("This section describes the information on the input, transformed into data as described by ", 
                      a("UCI Machine Learning Repository",href="https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.names")),                
                  br(),
                  div("Using the selection box on the left, you can choose which columns to display from the input information"),
+                 br(),
+                 h2("Data Dictionary"),                 
+                 div("41 continuous real [0,100] attributes of type [WORD] = percentage of words in the e-mail that match WORD, i.e. 100 * (number of times the WORD appears in the e-mail) total number of words in e-mail."),
+                 div("For example the column name money indicates the word money occuring in the text"),
+                 br(),
+                 div("7 continuous real [0,100] attributes of type num[NUMBER] = percentage of words in the e-mail that match WORD, i.e. 100 * (number of times the WORD appears in the e-mail) total number of words in e-mail. "),
+                 div("For example num857 indicates the number 857 occuring in the text"),
+                 br(),
+                 div("6 continuous real [0,100] attributes of type char[CHARACTER] = percentage of characters in the e-mail that match CHAR,i.e. 100 * (number of CHAR occurences) / total characters in e-mail"),
+                 div("For example charExclaimation indicates the percentage of exclaimation marks over the total number of characters"),
+                 br(),
+                 div("capitalAve - 1 continuous real [1,...] attribute, of the average length of uninterrupted sequences of capital letters."),
+                 br(),
+                 div("capitalLong - 1 continuous integer [0,...] attribute, of the longest uninterrupted sequence of capital letters"),
+                 br(),
+                 div("capitalTotal - 1 continuous integer [0,...] attribute, of the total number of capital letters in the email"),
                  br(),
                  fluidPage(
                    titlePanel("Information on the input text"),
@@ -55,6 +72,22 @@ shinyUI(pageWithSidebar(
                  br(),
                  div("Using the selection box on the left, you can choose which columns to display from the original spam data"),
                  br(),
+                 h2("Data Dictionary"),                 
+                 div("41 continuous real [0,100] attributes of type [WORD] = percentage of words in the e-mail that match WORD, i.e. 100 * (number of times the WORD appears in the e-mail) total number of words in e-mail."),
+                 div("For example the column name money indicates the word money occuring in the text"),
+                 br(),
+                 div("7 continuous real [0,100] attributes of type num[NUMBER] = percentage of words in the e-mail that match WORD, i.e. 100 * (number of times the WORD appears in the e-mail) total number of words in e-mail. "),
+                 div("For example num857 indicates the number 857 occuring in the text"),
+                 br(),
+                 div("6 continuous real [0,100] attributes of type char[CHARACTER] = percentage of characters in the e-mail that match CHAR,i.e. 100 * (number of CHAR occurences) / total characters in e-mail"),
+                 div("For example charExclaimation indicates the percentage of exclaimation marks over the total number of characters"),
+                 br(),
+                 div("capitalAve - 1 continuous real [1,...] attribute, of the average length of uninterrupted sequences of capital letters."),
+                 br(),
+                 div("capitalLong - 1 continuous integer [0,...] attribute, of the longest uninterrupted sequence of capital letters"),
+                 br(),
+                 div("capitalTotal - 1 continuous integer [0,...] attribute, of the total number of capital letters in the email"),
+                 br(),                 
                  fluidPage(
                    titlePanel("Information on the original spam data"),
                    fluidRow(
